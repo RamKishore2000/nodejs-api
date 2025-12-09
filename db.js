a201e27a2cfde7cd4a4f4c2d03e5305245bdfe15
ramkishore1234 <basavakishore65@gmail.com>
2025-12-09 14:52:39 +0530

    updated for railway

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-  host: "mysql.railway.internal",
-  user: "root",
-  password: "jCTuhNAUrmmxSfkEYAOFvsAvCudiIlCA",
-  database: "railway",
-  port: 3306
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT
 });
 
 db.connect((err) => {
